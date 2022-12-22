92ebc6c740051bc58d515aa0bc555006ac942b2d
vishalicious213 <vishalicious213@gmail.com>
2022-12-21 22:29:55 -0800

    button selects fighter names from new array

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
 
 let singhs = [
-    {name: "ella", img: ""},
-    {name: "kara", img: ""},
-    {name: "vish", img: ""},
+    {name: "ella", img: "face.jpg"},
+    {name: "kara", img: "face.jpg"},
+    {name: "vish", img: "face.jpg"},
 ]
 
 let stageEl = document.getElementById("stage")
 let fightButton = document.getElementById("fightButton")
 
 fightButton.addEventListener("click", function() {
-    let player1 = Math.floor(Math.random() * 17)
-    let player2 = Math.floor(Math.random() * 17)
-    stageEl.textContent = `${fighters[player1]} vs ${fighters[player2]}`
+    let player1 = Math.floor(Math.random() * singhs.length)
+    let player2 = Math.floor(Math.random() * singhs.length)
+    stageEl.textContent = `${singhs[player1].name} vs ${singhs[player2].name}`
 })
