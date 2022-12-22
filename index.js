cf463cab0e13bac1346a149fc7ea4f5bbceb254e
vishalicious213 <vishalicious213@gmail.com>
2022-12-22 13:30:53 -0800

    pulled player index calculation out and made it a function. added while loop that calls it to stop player1 & player2 from having same character

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let singhs = [
     {name: "oxy", img: "/img/oxy.jpg"},
     {name: "vish", img: "/img/vish.jpg"},
     {name: "Z", img: "/img/z.jpg"},
-    {name: "Ziggy", img: "/img/ziggy.jpg"},
+    {name: "ziggy", img: "/img/ziggy.jpg"},
 ]
 
 let stageEl = document.getElementById("stage")
@@ -18,9 +18,17 @@ let player2pic = document.getElementById("player-2-img")
 let player1name = document.getElementById("player-1-name")
 let player2name = document.getElementById("player-2-name")
 
+function rollNum() {
+    return Math.floor(Math.random() * singhs.length)
+}
+
 fightButton.addEventListener("click", function() {
-    let player1 = Math.floor(Math.random() * singhs.length)
-    let player2 = Math.floor(Math.random() * singhs.length)
+    let player1 = rollNum()
+    let player2 = rollNum()
+
+    while (player1 === player2) {
+        player2 = rollNum()
+    }
 
     player1pic.src = `${singhs[player1].img}`
     player2pic.src = `${singhs[player2].img}`
